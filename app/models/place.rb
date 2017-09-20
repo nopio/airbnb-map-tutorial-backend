73b75d08ef138a5229692b61e57f8c1fce85f6c2
Piotr Jaworski <piotrek.jaw@gmail.com>
2017-09-20 23:37:22 +0200

    API results limit

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,11 @@ class Place < ApplicationRecord
   scope :by_longitude, -> (min, max) { min && max ? where('longitude >= :min AND longitude <= :max', min: min, max: max) : all }
   scope :by_latitude, -> (min, max) { min && max ? where('latitude >= :min AND latitude <= :max', min: min, max: max) : all }
 
+  API_RESULTS_LIMIT = 100
+
   def self.search(min_lat:, max_lat:, min_lng:, max_lng:)
     by_latitude(min_lat, max_lat).
-      by_longitude(min_lng, max_lng)
+      by_longitude(min_lng, max_lng).
+      limit(API_RESULTS_LIMIT)
   end
 end
